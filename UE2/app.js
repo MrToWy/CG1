01649fee297d75ae6dee7a9db12f606099fa4015
Tobias Wylega <tobias@wylega.de>
2022-03-17 09:51:31 +0100

    UE2

--- UE2/app.js
+++ UE2/app.js
@@ -2,6 +2,10 @@
 
 const vertexShaderText =
     `
+    uniform vec3 color;
+    uniform vec2 offsetPos;
+    uniform float angle;
+    
     precision mediump float;
     
     attribute vec2 vertPosition;
@@ -10,11 +14,14 @@ const vertexShaderText =
     varying vec3 fragColor;
     
     void main(){
-        fragColor = vec3(0.1, 1.0, 0.0);
-        gl_Position = vec4(vertPosition, 0.0, 1.0);
+        fragColor = color;
+        
+        gl_Position = vec4(vertPosition + offsetPos, 0.0, 1.0);
+        
+        gl_Position[0] = vertPosition[0] * cos(angle) - vertPosition[1] * sin(angle);
+        gl_Position[1] = vertPosition[0] * sin(angle) + vertPosition[1] * cos(angle);
     }
-    
-    `;
+`;
 
 const fragmentShaderText =
     `
@@ -28,8 +35,6 @@ const fragmentShaderText =
     `;
 
 function init(){
-    console.log("hi");
-
 
     const triangle = document.getElementById("triangle")
     const gl = triangle.getContext("webgl");
@@ -64,22 +69,62 @@ function init(){
         return;
     }
 
-    gl.clearColor(1.0, 0.0, 0.7, 1);
+    gl.clearColor(0.0, 0.0, 0.0, 1);
 
     // prepare triangle
+    
+    const top_limit = 0.8;
+    const bot_limit = -0.85;
+    const left_limit = -0.4;
+    const right_limit = 0.4;
+    
+    const letter_bot = -0.5;
+    const letter_mid_height = 0.1;
+    
+    const width = 0.2;
+    
     const triangleVertices=[
-        0.0, 0.5,
-        -0.5, -0.8,
-        0.5, -0.8,
-        -0.9, 0.9,
-        -0.1, 0.9,
-        -0.5, 0.4,
-        0.9, 0.9,
-        0.1, 0.9,
-        0.5, 0.4,
-        0.9, -0.9,
-        0.9, -0.1,
-        0.4, -0.5
+
+
+        // right line
+        right_limit, top_limit, 
+        right_limit - width, letter_bot,
+        right_limit, letter_bot, 
+
+        right_limit - width, top_limit, 
+        right_limit, top_limit, 
+        right_limit - width, letter_bot,
+
+
+        // left line
+        left_limit, top_limit,
+        left_limit + width, letter_bot,
+        left_limit, letter_bot,
+
+        left_limit + width, top_limit,
+        left_limit, top_limit,
+        left_limit + width, letter_bot,
+
+
+        // mid line
+        right_limit, letter_mid_height + width, // top right
+        left_limit, letter_mid_height,// bot left
+        right_limit, letter_mid_height, // bot right
+
+        left_limit, letter_mid_height + width, // top left
+        right_limit, letter_mid_height + width, // top right
+        left_limit, letter_mid_height, // bot left
+
+
+
+        // bot line
+        right_limit, bot_limit + width, // top right
+        left_limit, bot_limit,// bot left
+        right_limit, bot_limit, // bot right
+        
+        left_limit, bot_limit + width, // top left
+        right_limit, bot_limit + width, // top right
+        left_limit, bot_limit // bot left
     ];
 
     const triangleVBO = gl.createBuffer();
@@ -91,6 +136,19 @@ function init(){
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
+
+    // RGB: 220,60,5
+    const colorAttribLocation = gl.getUniformLocation(program, "color");
+    gl.uniform3f(colorAttribLocation, 220/256, 60/256, 5/256);
+    
+    // angle
+    const angleAttribLocation = gl.getUniformLocation(program, "angle");
+    gl.uniform1f(angleAttribLocation, 200);
+    
+    // translation
+    const offsetAttribLocation = gl.getUniformLocation(program, "offsetPos");
+    gl.uniform2f(offsetAttribLocation, 0.9,0.9);
+    
     const positionAttributeLocation = gl.getAttribLocation(program, "vertPosition");
     gl.vertexAttribPointer(positionAttributeLocation,
         2, gl.FLOAT, false,
@@ -101,6 +159,29 @@ function init(){
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length/2);
 
+    let counter = 0;
+    let change = 1;
+    
+    function loop() {
+        // Code, der pro Frame ausgeführt wird        
+        counter += change;
+
+
+        gl.uniform3f(colorAttribLocation, counter/256, counter/256, counter/256);
+        
+        if(counter > 250 || counter < 1)
+            change = change*-1;
+
+        gl.uniform2f(offsetAttribLocation, 0.001 * counter,0.001 * counter);
+
+        gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length/2);
+
+        gl.uniform1f(angleAttribLocation, counter / 150);
+        
+        requestAnimationFrame(loop);
+    };
+    
+    requestAnimationFrame(loop);
 }
 
 window.onload = init;
