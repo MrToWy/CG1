f5736840e18756eadad73b144eeeab3f251fa876
Tobias Wylega <tobias@wylega.de>
2022-03-28 11:14:52 +0200

    nr4

--- UE3/experiment4.js
+++ UE3/experiment4.js
@@ -18,16 +18,18 @@ const fragmentShaderText =
 `
 precision mediump float;
 varying float fragValue;
-uniform vec3 fragColor1;
-uniform vec3 fragColor2;
 
 void main(){
-    vec3 color1 = vec3(0.0,0.0,1.0);
-    vec3 color2 = vec3(1.0,0.0,0.0);
-    vec3 fragColor = mix(color1, color2, fragValue);
-    fragColor[0] = smoothstep(0.4,0.6,fragColor[0]);
-    fragColor[1] = smoothstep(0.4,0.6,fragColor[1]);
-    fragColor[2] = smoothstep(0.4,0.6,fragColor[2]);
+    
+    vec3 color1 = 1==1 ? vec3(1.0, 0.0, 0.0) : vec3(0.0, 1.0, 0.0);
+    vec3 color2 = 2==1 ? vec3(1.0, 0.0, 0.0) : vec3(0.0, 1.0, 0.0);
+    
+    vec3 fragColor = mix(color1, color2, step(0.0, sin(gl_FragCoord.x * 0.016)));
+    
+    
+   // fragColor[0] = smoothstep(0.4,0.6,fragColor[0]);
+    //fragColor[1] = smoothstep(0.4,0.6,fragColor[1]);
+    //fragColor[2] = smoothstep(0.4,0.6,fragColor[2]);
     gl_FragColor = vec4(fragColor, 1.0);
 }
 `;
