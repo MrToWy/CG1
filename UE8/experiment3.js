1543371843af202ae29da0b0160e0284ad17ebcf
Tobias Wylega <tobias@wylega.de>
2022-05-16 11:37:21 +0200

    ue8

--- UE8/experiment3.js
+++ UE8/experiment3.js
@@ -1,93 +1,118 @@
-'use strict'
+'use strict';
 
+function objToVBO(objString){
+
+    let objArray = objString.split("\n");
+    let vbo = [];
+    let indicies = [];
+
+    for (let i = 0; i < objArray.length; i++) {
+        let line = objArray[i];
+
+        let columns = line.split(" ");
+        let prefix = columns[0];
+
+        if (prefix === "v"){
+            let x = columns[1];
+            let y = columns[2];
+            let z = columns[3];
+
+            vbo.push([x, y, z]);
+        }
+        else if(prefix === "f"){
+            let x = columns[1].split("/");
+            let y = columns[2].split("/");
+            let z = columns[3].split("/");
+        }
+
+    }
+
+    return vbo;
+}
 
 async function init(){
-
-
     let vertextRequest = await fetch("vertexShader.vert");
     let vertexShaderText = await vertextRequest.text();
 
     let fragmentRequest = await fetch("fragmentShader.frag");
     let fragmentShaderText = await fragmentRequest.text();
-    
-    
-    
-    console.log("Wir malen ein Dreieck!");
-    const canvas = document.getElementById("cg1");
-    const gl = canvas.getContext('webgl');
+
+    let teapotRequest = await fetch("teapot.obj");
+    let teapotText = await teapotRequest.text();
+
+
+    const triangle = document.getElementById("triangle")
+    const gl = triangle.getContext("webgl");
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderText);
     gl.compileShader(vertexShader);
 
-
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderText);
     gl.compileShader(fragmentShader);
+    // TODO: Check compile status
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.error('ERROR', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
 
-        if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS))
-            console.error('ERROR', gl.getShaderInfoLog(vertexShader));
-            
-        
-        if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS))
-            console.error('ERROR', gl.getShaderInfoLog(fragmentShader));
-    
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.error('ERROR', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
     const program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
+
     gl.validateProgram(program);
 
-    gl.clearColor(1.0, 0.8, 0.9, 1.0);
+    if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+        console.error('ERROR', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    gl.clearColor(1.0, 0.0, 0.7, 1);
 
     // prepare triangle
-    const traingleVertices = [
-        -0.9, 0.9, 0, 
-        -0.9, 0.7, 0,
-        0.9, 0.7, 1,
-        -0.9, 0.9, 0,
-        0.9, 0.9, 1,
-        0.9, 0.7, 1
-    ];
-
+    const triangleVertices = await fetchModel("teapot.obj");
+    console.log(triangleVertices);
+    
 
     const triangleVBO = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
-    gl.bufferData(gl.ARRAY_BUFFER, 
-        new Float32Array(traingleVertices), 
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices),
         gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
-    const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-    gl.vertexAttribPointer(
-        positionAttribLocation,
-        2, // dimension
-        gl.FLOAT,
-        gl.FALSE,
-        3 * Float32Array.BYTES_PER_ELEMENT,
-        0
-    );
-    const colorAttribLocation = gl.getAttribLocation(program,'color');
-
-    gl.vertexAttribPointer(
-        colorAttribLocation,
-        1,
-        gl.FLOAT,
-        gl.FALSE,
-        3 * Float32Array.BYTES_PER_ELEMENT,
-        2 * Float32Array.BYTES_PER_ELEMENT
-    )
     
-    gl.enableVertexAttribArray(positionAttribLocation);
-    gl.enableVertexAttribArray(colorAttribLocation);
+    const positionAttributeLocation = gl.getAttribLocation(program, "vertPosition");
+    gl.vertexAttribPointer(positionAttributeLocation,
+        3, gl.FLOAT, false,
+        8 * Float32Array.BYTES_PER_ELEMENT,
+        0);
 
+    const colorAttributeLocation = gl.getAttribLocation(program, "vertColor");
+    gl.vertexAttribPointer(colorAttributeLocation,
+        3, gl.FLOAT, false,
+        8 * Float32Array.BYTES_PER_ELEMENT,
+        5 * Float32Array.BYTES_PER_ELEMENT);
 
     // draw triangle
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.enableVertexAttribArray(colorAttributeLocation);
     
+    //gl.enable(gl.DEPTH_TEST);
+        
+    
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, triangleVertices.length/8);
+
 }
 
 window.onload = init;
+
