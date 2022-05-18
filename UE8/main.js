c2c2d220648c5d50058faa4cfeb31e48857a92ed
Tobias Wylega <tobias.wylega@stud.hs-hannover.de>
2022-05-18 18:10:15 +0200

    teapot example finished

--- UE8/main.js
+++ UE8/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function objToVBO(objString){
+function objToVBO(objString) {
 
     let objArray = objString.split("\n");
     let v = [];
@@ -14,37 +14,34 @@ function objToVBO(objString){
         let columns = line.split(" ");
         let prefix = columns[0];
 
-        if (prefix === "v"){
+        if (prefix === "v") {
             let x = parseFloat(columns[1]);
             let y = parseFloat(columns[2]);
             let z = parseFloat(columns[3]);
 
             v.push([x, y, z]);
-        }
-        else if(prefix === "vt"){
+        } else if (prefix === "vt") {
             let x = parseFloat(columns[1]);
             let y = parseFloat(columns[2]);
 
             vt.push([x, y]);
-        }
-        else if(prefix === "vn"){
+        } else if (prefix === "vn") {
             let x = parseFloat(columns[1]);
             let y = parseFloat(columns[2]);
             let z = parseFloat(columns[3]);
 
             vn.push([x, y, z]);
-        }
-        else if(prefix === "f"){
+        } else if (prefix === "f") {
 
             for (let j = 1; j < 4; j++) {
                 let triplet = columns[j].split("/");
 
-                let verticiesIndex = triplet[0]-1;
-                let texturesIndex = triplet[1]-1;
-                let normalIndex = triplet[2]-1;
+                let verticesIndex = triplet[0] - 1;
+                let texturesIndex = triplet[1] - 1;
+                let normalIndex = triplet[2] - 1;
 
                 // push vertices
-                vbo.push(v[verticiesIndex][0], v[verticiesIndex][1], v[verticiesIndex][2]);
+                vbo.push(v[verticesIndex][0], v[verticesIndex][1], v[verticesIndex][2]);
 
                 // push textures
                 vbo.push(vt[texturesIndex][0], vt[texturesIndex][1]);
@@ -59,7 +56,9 @@ function objToVBO(objString){
     return vbo;
 }
 
-async function init(){
+async function init() {
+
+
     let vertextRequest = await fetch("vertexShader.vert");
     let vertexShaderText = await vertextRequest.text();
 
@@ -75,7 +74,7 @@ async function init(){
 
     const triangle = document.getElementById("triangle")
     const gl = triangle.getContext("webgl");
-    
+
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderText);
     gl.compileShader(vertexShader);
@@ -84,12 +83,12 @@ async function init(){
     gl.shaderSource(fragmentShader, fragmentShaderText);
     gl.compileShader(fragmentShader);
     // TODO: Check compile status
-    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
         console.error('ERROR', gl.getShaderInfoLog(vertexShader));
         return;
     }
 
-    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
         console.error('ERROR', gl.getShaderInfoLog(fragmentShader));
         return;
     }
@@ -101,15 +100,13 @@ async function init(){
 
     gl.validateProgram(program);
 
-    if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
         console.error('ERROR', gl.getProgramInfoLog(program));
         return;
     }
 
     gl.clearColor(0.0, 1.0, 0.0, 1);
 
-    
-    
 
     const triangleVBO = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
@@ -120,7 +117,7 @@ async function init(){
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
-    
+
     const positionAttributeLocation = gl.getAttribLocation(program, "vertPosition");
     gl.vertexAttribPointer(positionAttributeLocation,
         3, gl.FLOAT, false,
@@ -136,12 +133,49 @@ async function init(){
     // draw triangle
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.enableVertexAttribArray(colorAttributeLocation);
-    
-    //gl.enable(gl.DEPTH_TEST);
-        
-    
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length/8);
 
+    gl.enable(gl.DEPTH_TEST);
+
+
+    let counter = 0;
+
+    function loop() {
+        counter -= 1;
+
+        let matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+        let matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+        let matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+        let matTranslateUniformLocation = gl.getUniformLocation(program, 'mTranslate');
+
+        let identityMatrix = new glMatrix.mat4.create();
+        let worldMatrix = new glMatrix.mat4.create();
+        let viewMatrix = new glMatrix.mat4.create();
+        let projMatrix = new glMatrix.mat4.create();
+        let translateMatrix = new glMatrix.mat4.create();
+
+        identity(identityMatrix);
+
+        rotateY(translateMatrix, identityMatrix, counter * Math.PI / 180);
+        translate(translateMatrix, translateMatrix, [0, -0.4, 0])
+        scale(translateMatrix, translateMatrix, [1, 1, 1]);
+
+        let eye = [1, 2, 10];
+        lookAt(viewMatrix, eye, [0, 0, 0], [0, 1, 0]);
+
+        perspective(projMatrix, 45 * Math.PI / 180, triangle.clientWidth / triangle.clientHeight, 0.1, 1000.0);
+
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+        gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+        gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+        gl.uniformMatrix4fv(matTranslateUniformLocation, gl.FALSE, translateMatrix);
+
+
+        gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length / 8);
+        
+        requestAnimationFrame(loop);
+    }
+
+    requestAnimationFrame(loop);
 }
 
 window.onload = init;
--- UE8/vertexShader.vert
+++ UE8/vertexShader.vert
@@ -3,9 +3,14 @@ precision mediump float;
 attribute vec3 vertPosition;
 attribute vec3 vertColor;
 
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
+uniform mat4 mTranslate;
+
 varying vec3 fragColor;
 
 void main(){
     fragColor = vertColor;
-    gl_Position = vec4(vertPosition, 1.0);
+    gl_Position = mProj * mView *   mTranslate * mWorld * vec4(vertPosition, 1.0);
 }
